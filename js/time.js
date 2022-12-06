2335d15e6ccb59984ce9c6095048d9c578b56dc0
qweyaw <2270096623@qq.com>
2022-12-06 15:36:59 +0800

    feat: add time

---
+++ js/time.js
@@ -0,0 +1,22 @@
+const timeEle = document.querySelector(".time")
+
+const time = () => {
+  const loveDate = new Date("2021/10/18").getTime()
+  const nowDate = new Date().getTime()
+  const day = Math.floor((nowDate - loveDate) / 1000 / 60 / 60 / 24)
+  const hour = Math.floor(((nowDate - loveDate) / 1000 / 60 / 60) % 24)
+  const minute = Math.floor(((nowDate - loveDate) / 1000 / 60) % 60)
+  const second = Math.floor(((nowDate - loveDate) / 1000) % 60)
+  return `
+    <div class="timeText">
+      相恋
+      <p>${day} 天</p>
+      <p>${hour} 时</p>
+      <p>${minute} 分</p>
+      <p>${second} 秒</p>
+    </div>
+  `
+}
+setInterval(() => {
+  timeEle.innerHTML = time()
+}, 1000)
